1877612cdd83056c1779d321f52ff1becb843b94
mas <m@stdwtr.io>
2022-04-19 17:00:49 -0400

    Add exp function and ln

--- math/func.cpp
+++ math/func.cpp
@@ -18,17 +18,24 @@ double taylor(double x, int n)
 double func::sqrt(double x)
 {
     if (x < 0)
+    {
         throw std::invalid_argument("sqrt: negative argument");
+    }
     if (x == 0)
+    {
         return 0;
+    }
     double xn = x;
     double eps = 1e-15;
     double last = xn;
-    while ((xn * xn - x) > eps) {
+    while ((xn * xn - x) > eps)
+    {
         last = xn;
         xn = (xn + (x / xn)) / 2;
-        if (xn == last) // sometimes convergence is not reached
+        if (xn == last)
+        { // sometimes convergence is not reached
             break;
+        }
     }
     return xn;
 }
@@ -42,12 +49,13 @@ double func::Q_rsqrt(double number)
     const double threehalfs = 1.5F;
 
     x2 = number * 0.5F;
-    y  = number;
-    i  = * ( long * ) &y;                       // evil floating point bit level hacking
-    i  = 0x5f3759df - ( i >> 1 );               // what the fuck?
-    y  = * ( double * ) &i;
-    y  = y * ( threehalfs - ( x2 * y * y ) );   // 1st iteration
-    y  = y * ( threehalfs - ( x2 * y * y ) );   // 2nd iteration, this can be removed
+    y = number;
+    i = *(long *) &y;                       // evil floating point bit level hacking
+    i = 0x5f3759df - (i >> 1);               // what the fuck?
+    y = *(double *) &i;
+    y = y * (threehalfs - (x2 * y * y));   // 1st iteration
+    y = y *
+        (threehalfs - (x2 * y * y));   // 2nd iteration, this can be removed
     return y;
 }
 
@@ -56,7 +64,9 @@ double func::Q_sqrt(double number)
     // Hey, Copilot figured out how to do a reciprocal.
     double q_rsqrt = func::Q_rsqrt(number);
     if (q_rsqrt == 0)
+    {
         return 0;
+    }
     return 1.0 / q_rsqrt;
 }
 
@@ -69,22 +79,51 @@ double func::pow(double x, double y)
 double func::exp(double x)
 {
     if (x == 0)
+    {
         return 1;
+    }
     double factorial = 1;
     double power = 1;
     double sum = 1;
-    for (int i = 1; i < 21; i++) {
+    double lastn = 1;
+    double inf = std::numeric_limits<double>::infinity();
+    for (int i = 1; i < 1000; i++)
+    {
+        lastn = sum;
         power *= x;
         factorial *= i;
         sum += power / factorial;
+        if (sum == lastn)
+        {
+            return sum;
+        } else if (sum == inf)
+        {
+            return lastn;
+        }
     }
     return sum;
 }
 
 double func::ln(double x)
 {
-    throw std::invalid_argument("Not implemented");
-    return 0;
+    if (x == 1)
+    {
+        return 0;
+    }
+    if (x < 0)
+    {
+        throw std::invalid_argument("ln: negative argument");
+    }
+    double yn = x - 1.0;
+    double yn1 = yn;
+    double expon = 1;
+    do
+    {
+        yn = yn1;
+        expon = func::exp(yn);
+        yn1 = yn + 2 * (x - expon) / (x + expon);
+    } while (func::abs(yn1 - yn) > 1e-15);
+    return yn;
 }
 
 double func::log(double x)
--- tests/func_tests.cpp
+++ tests/func_tests.cpp
@@ -3,7 +3,7 @@
 //
 
 #include <iostream>
-#include <math.h>
+#include <cmath>
 #include "func_tests.h"
 #include "func.h"
 
@@ -22,7 +22,7 @@ bool test_Q_sqrt()
     double eps = 1e-15;
     for (int i = 0; i < 100; i++)
     {
-        tmp = (func::Q_sqrt(i) - std::sqrt(i)) < eps;
+        tmp = func::abs(func::Q_sqrt(i) - std::sqrt(i)) < eps;
         success &= tmp;
         if (!tmp)
         {
@@ -32,6 +32,7 @@ bool test_Q_sqrt()
                    15, func::Q_sqrt(i) - std::sqrt(i));
         }
     }
+    return success;
 }
 
 bool test_Q_rsqrt()
@@ -49,12 +50,14 @@ bool test_Q_rsqrt()
 
 bool test_sqrt()
 {
-    bool tmp = true;
+    bool tmp;
     bool success = true;
-    for (int i = 0; i < 1000; i++) {
-        tmp = (func::sqrt(i) - std::sqrt(i)) < 1e-14;
+    for (int i = 0; i < 1000; i++)
+    {
+        tmp = func::abs(func::sqrt(i) - std::sqrt(i)) < 1e-14;
         success &= tmp;
-        if (!tmp) {
+        if (!tmp)
+        {
             printf("sqrt(%d) = %.*f, std::sqrt(%d) = %.*f\n, diff = %.*f\n",
                    i, 15, func::sqrt(i),
                    i, 15, std::sqrt(i),
@@ -85,14 +88,37 @@ bool test_polynomial()
 bool test_exp()
 {
     bool success = func::exp(0) == 1;
-    for (int i = 1; i < 100; i++) {
-        bool tmp = (func::exp(i) - std::exp(i)) < 1e-15;
+    for (int i = 1; i < 15; i++)
+    {
+        bool tmp = func::abs(func::exp(i) - std::exp(i)) < 1e-9;
         success &= tmp;
-        if (!tmp) {
+        if (!tmp)
+        {
             printf("exp(%d) = %.*f, std::exp(%d) = %.*f, diff = %.*f\n",
                    i, 15, func::exp(i),
                    i, 15, std::exp(i),
                    15, func::exp(i) - std::exp(i));
+        }
+    }
+    return success;
+}
+
+bool test_ln()
+{
+    bool success = func::ln(1) == 0;
+    bool tmp = true;
+    for (int i = 1; i < 15; i++)
+    {
+        double a = func::ln(i);
+        double b = std::log(i);
+        tmp = func::abs(a - b) < 1e-15;
+        success &= tmp;
+        if (!tmp)
+        {
+            printf("ln(%d) = %.*f, std::ln(%d) = %.*f, diff = %.*f\n",
+                   i, 15, a,
+                   i, 15, b,
+                   15, a - b);
         }
     }
     return success;
@@ -107,6 +133,7 @@ Harness func_tests()
     harness.run_test(test_pow_long, "pow_long");
     harness.run_test(test_polynomial, "polynomial");
     harness.run_test(test_exp, "exp");
+    harness.run_test(test_ln, "ln");
     /* Expect failure
     harness.run_test(test_Q_sqrt, "Q_sqrt");
     harness.run_test(test_Q_rsqrt, "Q_rsqrt");
